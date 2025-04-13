9e8767f0b271748c9c80a8fb6f3dbfe673475b04
Vladyslav Korin <korinvlad2@gmail.com>
2025-04-13 09:29:11 +0000

    fix

--- index.js
+++ index.js
@@ -89,27 +89,12 @@ app.post('/api/users/:_id/exercises', async (req, res) => {
 });
 
 
-// TODO 
-// logs in format 
-/**
- * {
-  username: "fcc_test",
-  count: 1,
-  _id: "5fb5853f734231456ccb3b05",
-  log: [{
-    description: "test",
-    duration: 60,
-    date: "Mon Jan 01 1990",
-  }]
-}
- * 
- */
 app.get('/api/users/:_id/logs', async (req, res) => {
   try {
     const userId = req.params._id;
-    const user = await User.findById(userId);
     const { from, to, limit } = req.query;
 
+    const user = await User.findById(userId);
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
@@ -118,8 +103,10 @@ app.get('/api/users/:_id/logs', async (req, res) => {
     if (from) dateFilter.$gte = new Date(from);
     if (to) dateFilter.$lte = new Date(to);
 
-    let query = { userId: userId };
-    if (from || to) query.date = dateFilter;
+    let query = { userId };
+    if (from || to) {
+      query.date = dateFilter;
+    }
 
     let exercisesQuery = Exercise.find(query);
 
@@ -127,20 +114,21 @@ app.get('/api/users/:_id/logs', async (req, res) => {
       exercisesQuery = exercisesQuery.limit(parseInt(limit));
     }
 
-    const exercises = await Exercise.find({ userId: userId });
+    const exercises = await exercisesQuery;
 
     const log = exercises.map(ex => ({
       description: ex.description,
       duration: ex.duration,
-      date: new Date(ex.date).toDateString(),
+      date: new Date(ex.date).toDateString()
     }));
 
     res.json({
       username: user.username,
-      count: exercises.length,
+      count: log.length,
       _id: userId,
-      log: log
+      log
     });
+
   } catch (e) {
     res.status(500).json({ error: 'Server error' });
   }
